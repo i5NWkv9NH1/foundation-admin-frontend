b0f3994adce72fe62d69a917c3fe69e442d6a996
i5NWkv9NH1 <sloananna326@gmail.com>
2024-08-11 00:44:34 +0900

    feat: add UserProfileMenu component at right position. fix: enhance build tree method and add model interface. style: format code.

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -1,38 +1,56 @@
 /* eslint-disable no-use-before-define */
 export interface BaseEntity {
-  id?: string
-  createdAt?: string
-  updatedAt?: string
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  [key: string]: any;
 }
 export interface Role extends BaseEntity {
-  name: string
-  code: string
-  actions: []
+  name: string;
+  code: string;
+  actions: Action[];
+  accounts: Account[];
 }
 export interface Menu extends BaseEntity {
-  label: string
-  router: string
-  icon: string
-  path: string
-  parentId: null | string
-  parent: Menu | null
-  children: Menu[]
-  actions: Action[]
+  label: string;
+  router: string;
+  icon: string;
+  path: string;
+  parentId: null | string;
+  parent: Menu | null;
+  children: Menu[];
+  actions: Action[];
 }
 export interface Action extends BaseEntity {
-  name: string
-  code: string
-  icon: string
-  menuId: string
-  // menu?: Menu
+  name: string;
+  code: string;
+  icon: string;
+  menuId: string;
 }
 
 export interface Account extends BaseEntity {
-  name: string
-  username: string
-  email: string
-  phone: string
-  address: string
-  avatarUrl: string
-  roles?: Role[]
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  address: string;
+  avatarUrl: string;
+  status: 'DISABLE' | 'ENABLE';
+  gender: 'MALE' | 'FEMALE' | 'PRIVATE';
+  roles?: Role[];
+  organizations: Organization[];
+}
+
+export interface Organization extends BaseEntity {
+  name: string;
+  code: string;
+  type: 'GROUP' | 'COMPANY' | 'DEPARTMENT';
+  status: 'DISABLE' | 'ENABLE';
+  sort: number;
+  icon: string;
+  path: string;
+  parentId: string | null;
+  parent: Organization;
+  children: Organization[];
+  accounts: Account[];
 }
--- src/types/response.ts
+++ src/types/response.ts
@@ -1,25 +1,25 @@
 export interface Response {
   meta?: {
-    processedBy: string
-    version: string
-  }
-  message: string
-  statusCode: number
-  errors?: any
-  timestamp: string // To include the response timestamp
+    processedBy: string;
+    version: string;
+  };
+  message: string;
+  statusCode: number;
+  errors?: any;
+  timestamp: string; // To include the response timestamp
 }
 export interface SystemResponse<T> extends Response {
-  result: T
+  result: T;
 }
 export interface PaginateMeta {
-  page: number
-  itemPerPage: number
-  itemsCount: number
-  pagesCount: number
+  page: number;
+  itemPerPage: number;
+  itemsCount: number;
+  pagesCount: number;
 }
 export interface SystemPaginateResponse<T> extends Response {
   result: {
-    items: T[]
-    meta: PaginateMeta
-  }
+    items: T[];
+    meta: PaginateMeta;
+  };
 }
