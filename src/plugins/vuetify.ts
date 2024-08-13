5eb7e7ac16da2524ed8b4d8fc6012a1fef319aac
i5NWkv9NH1 <sloananna326@gmail.com>
2024-08-13 10:23:18 +0900

    feat: setup themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -12,6 +12,8 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 // import { md3 } from 'vuetify/blueprints';
 import * as directives from 'vuetify/directives';
+import { OCEAN_BREEZE_LIGHT, OCEAN_BREEZE_DARK, FOREST_RETREAT_LIGHT, FOREST_RETREAT_DARK, SUNSET_GLOW_LIGHT, SUNSET_GLOW_DARK, GPT_LIGHT, GPT_DARK, TWITTER_LIGHT, TWITTER_DARK, GOOGLE_LIGHT, GOOGLE_DARK, FACEBOOK_LIGHT, FACEBOOK_DARK, PINTEREST_LIGHT, PINTEREST_DARK, LINKED_LIGHT, LINKED_DARK, SPOTIFY_LIGHT, SPOTIFY_DARK } from '@/themes';
+import { DEFAULT_LIGHT_THEME } from '@/constants';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -21,5 +23,31 @@ export default createVuetify({
   //     ripple: false,
   //   },
   // },
-  directives
+  directives,
+  theme: {
+    // ? preset light and dark
+    defaultTheme: DEFAULT_LIGHT_THEME,
+    themes: {
+      OCEAN_BREEZE_LIGHT,
+      OCEAN_BREEZE_DARK,
+      FOREST_RETREAT_LIGHT,
+      FOREST_RETREAT_DARK,
+      SUNSET_GLOW_LIGHT,
+      SUNSET_GLOW_DARK,
+      GPT_LIGHT,
+      GPT_DARK,
+      TWITTER_LIGHT,
+      TWITTER_DARK,
+      GOOGLE_LIGHT,
+      GOOGLE_DARK,
+      FACEBOOK_LIGHT,
+      FACEBOOK_DARK,
+      PINTEREST_LIGHT,
+      PINTEREST_DARK,
+      LINKED_LIGHT,
+      LINKED_DARK,
+      SPOTIFY_LIGHT,
+      SPOTIFY_DARK
+    }
+  }
 });
